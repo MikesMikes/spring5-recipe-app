9004bdaa6088c3bbe38cb06d41772a45a3f3f4f0
MikesMikes <michaelkaliber15@gmail.com>
2022-03-17 17:13:03 +0000

    Recipe Category IDs joined

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -30,6 +30,9 @@ public class Recipe {
     private Set<Ingredient> ingredient;
 
     @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories;
 
 
