e207c2416ee9da15b36759f772ef077cee99c46f
MikesMikes <michaelkaliber15@gmail.com>
2022-04-19 16:35:07 +0100

    list view for recipe ingredients

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -1,11 +1,24 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IngredientController {
 
-    public String getIngredients(){
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/{id}/ingredients")
+    public String getIngredients(@PathVariable String id, Model model){
+
+        model.addAttribute("recipe", recipeService.findRecipeCommandById(Long.valueOf(id)));
 
         return "recipe/ingredient/list";
     }
